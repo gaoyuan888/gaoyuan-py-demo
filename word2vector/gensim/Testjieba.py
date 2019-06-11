bf86332a244190f13365d29dd78dc075391f634a
yaoyizhou1 <yauri.zhang@qunar.com>
2019-06-11 20:58:01 +0800

    init

--- word2vector/gensim/Testjieba.py
+++ word2vector/gensim/Testjieba.py
@@ -1,18 +1,39 @@
+import codecs
+import re
+
 import jieba
 import jieba.analyse
-import jieba.posseg as pseg
-import codecs,sys
+
+# step 1 读取停用词
+stop_words = []
+with open('stop_words.txt',encoding= 'utf-8') as f:
+    line = f.readline()
+    while line:
+        stop_words.append(line[:-1])
+        line = f.readline()
+stop_words = set(stop_words)
+print('停用词读取完毕，共{n}个单词'.format(n=len(stop_words)))
+
 def cut_words(sentence):
-    #print sentence
+    # print sentence
     return " ".join(jieba.cut(sentence)).encode('utf-8')
-f=codecs.open('med.zh.txt','r',encoding="utf8")
-target = codecs.open("med.zh.seg.txt", 'w',encoding="utf8")
-print ('open files')
-line_num=1
+
+
+f = codecs.open('med.zh.txt', 'r', encoding="utf8")
+target = codecs.open("med.zh.seg.txt", 'w', encoding="utf8")
+print('open files')
+line_num = 1
 line = f.readline()
 while line:
     print('---- processing ', line_num, ' article----------------')
-    line_seg = " ".join(jieba.cut(line))
+    line = re.sub(" ", "", line)
+    raw_sentence=[]
+    raw_words=list(jieba.cut(line))
+    for word in raw_words:
+        if word not in stop_words and word not in ['qingkan520', 'www', 'com', 'http']:
+            raw_sentence.append(word)
+    # line_seg = " ".join(jieba.cut(line))
+    line_seg = " ".join(raw_sentence)
     target.writelines(line_seg)
     line_num = line_num + 1
     line = f.readline()
@@ -22,11 +43,11 @@ exit()
 while line:
     curr = []
     for oneline in line:
-        #print(oneline)
+        # print(oneline)
         curr.append(oneline)
     after_cut = map(cut_words, curr)
     target.writelines(after_cut)
-    print ('saved',line_num,'articles')
+    print('saved', line_num, 'articles')
     exit()
     line = f.readline1()
 f.close()
