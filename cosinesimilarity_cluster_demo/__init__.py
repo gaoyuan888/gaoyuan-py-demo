71d6ec4b4317c37da3f371fcf84472734cd18fb3
yaoyizhou1 <yauri.zhang@qunar.com>
2019-06-14 11:44:34 +0800

    init

--- cosinesimilarity_cluster_demo/__init__.py
+++ cosinesimilarity_cluster_demo/__init__.py
@@ -13,6 +13,18 @@ import re
 # 词频统计
 from collections import Counter
 
+
+def yuCaozuo(arr1, arr2):
+    l1 = arr2.__len__()
+    l2 = arr1.__len__()
+    if l1 == 0 or l2 == 0:
+        return []
+    res = [0] * l1
+    for index in range(l2):
+        res[index] = int(arr1[index]) & int(arr2[index])
+    return res
+
+
 if __name__ == '__main__':
 
     # 分词
@@ -38,7 +50,7 @@ if __name__ == '__main__':
     line_num = 1
     for line in doc_goodat_list:
         line_num = line_num + 1
-        print('---- processing ', line_num, ' article----------------')
+        # print('---- processing ', line_num, ' article----------------')
         line = re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）]+", "", line)
         raw_sentence = []
         raw_words = list(jieba.cut(line))
@@ -47,6 +59,7 @@ if __name__ == '__main__':
                 raw_sentence.append(word)
         line_seg = " ".join(raw_sentence)
         target.writelines(line_seg + "\n")
+    target.close()
 
     # step 3 读取分词后的数据
     # samples = ['种植 牙 牙周病 治疗 修复 及口 内 治疗', '乳腺 肿瘤 及 乳房 整形 领域 的 手术 消化系统 等 常见 肿瘤 的 诊治']
@@ -82,7 +95,7 @@ if __name__ == '__main__':
     row_index = -1
     for line_row in array_list:
         row_index = row_index + 1
-        print(row_index)
+        # print(row_index)
         column_index = 0
         for value in line_row:
             column_index = column_index + 1
@@ -118,42 +131,79 @@ if __name__ == '__main__':
         for sim_value in line:
             similar_df.writelines(str(sim_value) + "  ")
         similar_df.writelines("\n")
+    similar_df.close()
 
     # 组织一个map 进行分组
     cluster_tuple_list = []
     cluster_type = 0
     samples_size = samples.__len__()
-    cluster_type_dist = {}
+    one_hot_array = one_hot_results.tolist()
     for index_ in range(samples_size):
-        sentence_index = index_ + 1
+        cluster_onehot_list = []
+        for cluster in cluster_tuple_list:
+            onehot_sum_list = one_hot_array[cluster[0][0]]
+            for tuple_list in cluster:
+                onehot_sum_list = yuCaozuo(onehot_sum_list, one_hot_array[tuple_list[0]])
+            cluster_onehot_list.append(onehot_sum_list)
+
         if cluster_tuple_list.__len__() == 0:
-            idx_dep = (sentence_index, second_depart_name_list[index_], cluster_type,)
+            idx_dep = (index_, second_depart_name_list[index_], cluster_type,)
             type_list = [idx_dep]
             cluster_tuple_list.append(type_list)
         else:
-            # cluster_tuple_index = -1
             cluster_tuple_dict_tmp = {}
             for tup_list in cluster_tuple_list:
-                # cluster_tuple_index = cluster_tuple_index + 1
                 # 遍历元组中的行号
                 similarity_num = 0
                 total_num = 0
                 for tup_index in tup_list:
                     total_num = total_num + 1
-                    if similarities[tup_index[0]][index_] > 0.1:
+                    if similarities[tup_index[0]][index_] > 0.3:
                         similarity_num = similarity_num + 1
                 similarity_percent = similarity_num / total_num
                 cluster_tuple_dict_tmp[tup_list[0][2]] = similarity_percent
             dict_list = sorted(cluster_tuple_dict_tmp.items(), key=operator.itemgetter(1), reverse=True)
             # 判断当前要归类的节点和之前分组是否包含相同的词语，如果包含则加入，如果不包含，则新区分一个分组
-            if dict_list[0][1] > 0.5:
-                idx_dep = (sentence_index, second_depart_name_list[index_], dict_list[0][0],)
-                cluster_tuple_list[dict_list[0][0]].append(idx_dep)
-            else:
+            process_df = codecs.open("process_df.txt", 'w', encoding="utf8")
+            for dict in dict_list:
+                similar_words = yuCaozuo(cluster_onehot_list[dict[0]], one_hot_array[index_])
+                arr_clss = []
+                for idx in range(cluster_onehot_list[dict[0]].__len__()):
+                    if cluster_onehot_list[dict[0]][idx] == 1:
+                        arr_clss.append(tokenizer.index_word[idx])
+                # print("第{}类单词:{}".format(dict[0], arr_clss))
+                process_df.writelines("第{}类单词:{}".format(dict[0], arr_clss)+"\n")
+                arr_onehot = []
+                for idx in range(one_hot_array[index_].__len__()):
+                    if one_hot_array[index_][idx] == 1:
+                        arr_onehot.append(tokenizer.index_word[idx])
+                # print("第{}行单词:{}".format(index_, arr_onehot))
+                process_df.writelines("第{}行单词:{}".format(index_, arr_onehot) + "\n")
+                same_word_list = []
+                for idx in range(similar_words.__len__()):
+                    if similar_words[idx] == 1:
+                        same_word_list.append(tokenizer.index_word[idx])
+                # print("第{}行与第{}类的相似词为:{}".format(index_, dict[0], same_word_list))
+                process_df.writelines("第{}行与第{}类的相似词为:{}".format(index_, dict[0], same_word_list) + "\n")
+                process_df.writelines("===============")
+            process_df.close()
+
+            flag = True
+            for dict in dict_list:
+                similar_words = yuCaozuo(cluster_onehot_list[dict[0]], one_hot_array[index_])
+                same_words_count = sum(similar_words)
+                if dict[1] > 0.5 and same_words_count > 0:
+                    print("第{}行，加入第{}类,相似度:{}".format(index_, dict[0], dict[1]))
+                    idx_dep = (index_, second_depart_name_list[index_], dict[0],)
+                    cluster_tuple_list[dict[0]].append(idx_dep)
+                    flag = False
+                    break
+            if flag:
                 cluster_type += 1
-                idx_dep = (sentence_index, second_depart_name_list[index_], cluster_type,)
+                idx_dep = (index_, second_depart_name_list[index_], cluster_type,)
                 type_list = [idx_dep]
                 cluster_tuple_list.append(type_list)
+                print("第{}行，新增第{}类".format(index_, dict[0]))
 
     print(cluster_tuple_list)
 
