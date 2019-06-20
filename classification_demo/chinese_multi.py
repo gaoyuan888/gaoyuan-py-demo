676b9342a76ee1e10c7ff2865301c7e1a08892d5
yaoyizhou1 <yauri.zhang@qunar.com>
2019-06-20 18:48:44 +0800

    init

---
+++ classification_demo/chinese_multi.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+# Created by Jinkey on 2018/1/4.
+__author__ = 'Jinkey'
+
+import tensorflow as tf
+import jieba as jb
+import numpy as np
+import pandas as pd
+from classification_demo import utils
+
+titles = utils.load_data(catalogue=utils.MULTI_FLAG)
+target = utils.load_label(catalogue=utils.MULTI_FLAG)
+
+titles = []
+
+corpus_list = pd.read_csv("train_corpus_write.txt")
+class_list = corpus_list["class"]
+disease_desc_list = corpus_list["disease_desc"]
+diag_size = pd.read_csv("diagclass_total_write.txt").__len__()
+target = np.zeros(shape=[corpus_list.__len__(), diag_size])
+for index in range(corpus_list.__len__()):
+    titles.append(disease_desc_list[index])
+    type_ = class_list[index] - 1
+    target[index][type_] = 1
+
+max_sequence_length = 30
+embedding_size = 50
+
+# 标题分词
+titles = [".".join(jb.cut(t, cut_all=True)) for t in titles]
+
+# word2vec 词袋化
+vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor(max_sequence_length, min_frequency=1)
+text_processed = np.array(list(vocab_processor.fit_transform(titles)))
+
+# 读取标签
+dict = vocab_processor.vocabulary_._mapping
+sorted_vocab = sorted(dict.items(), key=lambda x: x[1])
+
+# 配置网络结构
+model = utils.build_netword(catalogue=utils.MULTI_FLAG, dict=dict, embedding_size=embedding_size,
+                            max_sequence_length=max_sequence_length)
+
+# # 训练模型
+model.fit(text_processed, target, batch_size=512, epochs=100, )
+# # 保存模型
+model.save("health_and_tech_design.h5")
+
+# 加载预训练的模型
+model.load_weights("health_and_tech_design.h5")
+
+# 预测样本
+sen = "您好肚子疼很难受谢谢医生了"
+sen_prosessed = " ".join(jb.cut(sen, cut_all=True))
+sen_prosessed = vocab_processor.transform([sen_prosessed])
+sen_prosessed = np.array(list(sen_prosessed))
+result = model.predict(sen_prosessed)
+
+catalogue = list(result[0]).index(max(result[0]))
+if max(result[0]) > 0.8:
+    if catalogue == 0:
+        print("第一类")
+    elif catalogue == 1:
+        print("第二类")
+    elif catalogue == 2:
+        print("第三类")
+    elif catalogue == 3:
+        print("第四类")
+    elif catalogue == 4:
+        print("第五类")
---
+++ classification_demo/utils.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Jinkey'
+
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+import keras as krs
+
+BINARY_FLAG = "binary"
+MULTI_FLAG = "multi"
+
+
+def load_data(catalogue=BINARY_FLAG):
+    titles = []
+    print("正在加载健康类别的数据...")
+    with open("data/health.txt", "r", encoding='utf-8') as f:
+        for line in f.readlines():
+            titles.append(line.strip())
+
+    print("正在加载科技类别的数据...")
+    with open("data/tech.txt", "r", encoding='utf-8') as f:
+        for line in f.readlines():
+            titles.append(line.strip())
+
+    if catalogue == MULTI_FLAG:
+        print("正在加载设计类别的数据...")
+        with open("data/design.txt", "r", encoding='utf-8') as f:
+            for line in f.readlines():
+                titles.append(line.strip())
+
+    print("一共加载了 %s 个标题" % len(titles))
+
+    return titles
+
+
+def load_label(catalogue=BINARY_FLAG):
+    if catalogue == BINARY_FLAG:
+        arr0 = np.zeros(shape=[12000, ])
+        arr1 = np.ones(shape=[12000, ])
+        target = np.hstack([arr0, arr1])
+        print("一共加载了 %s 个标签" % target.shape)
+        return target
+    elif catalogue == MULTI_FLAG:
+        arr0 = np.zeros(shape=[12000, ])
+        arr1 = np.ones(shape=[12000, ])
+        arr2 = np.array([2]).repeat(7318)
+        target = np.hstack([arr0, arr1, arr2])
+        print("一共加载了 %s 个标签" % target.shape)
+
+        encoder = LabelEncoder()
+        encoder.fit(target)
+        encoded_target = encoder.transform(target)
+        dummy_target = krs.utils.np_utils.to_categorical(encoded_target)
+
+        return dummy_target
+
+
+def build_netword(dict, catalogue=BINARY_FLAG, embedding_size=50, max_sequence_length=30):
+    if catalogue == BINARY_FLAG:
+        # 配置网络结构
+        model = krs.Sequential()
+        model.add(krs.layers.Embedding(len(dict.items()), embedding_size, input_length=max_sequence_length))
+        model.add(krs.layers.LSTM(32, dropout=0.2, recurrent_dropout=0.2))
+        model.add(krs.layers.Dense(1))
+        model.add(krs.layers.Activation("sigmoid"))
+        model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+        return model
+
+    elif catalogue == MULTI_FLAG:
+        # 配置网络结构
+        model = krs.Sequential()
+        # 将正整数（索引值）转换为固定尺寸的稠密向量。 例如： [[4], [20]] -> [[0.25, 0.1], [0.6, -0.2]]
+        # 该层只能用作模型中的第一层。
+        model.add(krs.layers.Embedding(len(dict.items()), embedding_size, input_length=max_sequence_length))
+        model.add(krs.layers.LSTM(32, dropout=0.2, recurrent_dropout=0.2))
+        model.add(krs.layers.Dense(7))
+        model.add(krs.layers.Activation("softmax"))
+        model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+        return model
