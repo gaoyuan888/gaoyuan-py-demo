e4faf58058b67be799882d079e60060b21e4b964
yaoyizhou1 <yauri.zhang@qunar.com>
2019-07-04 14:15:07 +0800

    Revert "init"
    This reverts commit c497609

--- naive_bayes_demo/news_c/news_c.py
+++ naive_bayes_demo/news_c/news_c.py
@@ -2,23 +2,24 @@
 import pandas as pd
 import jieba
 import numpy
+
 df_news = pd.read_table('./data/val.txt', names=['category', 'theme', 'URL', 'content'], encoding='utf-8')
 df_news = df_news.dropna()
 df_news.head()
-print (df_news.shape)
+print(df_news.shape)
 
 # 1.分词：使用结吧分词器 ###
 content = df_news.content.values.tolist()
-print (content[1000])
+print(content[1000])
 content_S = []
 for line in content:
     current_segment = jieba.lcut(line)
     if len(current_segment) > 1 and current_segment != '\r\n':  # 换行符
         content_S.append(current_segment)
 
-print (content_S[1000])
+print(content_S[1000])
 df_content = pd.DataFrame({'content_S': content_S})
-print (df_content.head())
+print(df_content.head())
 stopwords = pd.read_csv("stopwords.txt", index_col=False, sep="\t", quoting=3, names=['stopword'], encoding='utf-8')
 stopwords.head(20)
 
@@ -69,9 +70,9 @@ plt.imshow(wordcloud)
 import jieba.analyse
 
 index = 2400
-print (df_news['content'][index])
+print(df_news['content'][index])
 content_S_str = "".join(content_S[index])
-print ("  ".join(jieba.analyse.extract_tags(content_S_str, topK=5, withWeight=False)))
+print("  ".join(jieba.analyse.extract_tags(content_S_str, topK=5, withWeight=False)))
 
 # LDA ：主题模型
 # 格式要求：list of list形式，分词好的的整个语料
@@ -85,19 +86,19 @@ corpus = [dictionary.doc2bow(sentence) for sentence in contents_clean]
 lda = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=20)  # 类似Kmeans自己指定K值
 
 # 一号分类结果
-print (lda.print_topic(1, topn=5))
+print(lda.print_topic(1, topn=5))
 for topic in lda.print_topics(num_topics=20, num_words=5):
-    print (topic[1])
+    print(topic[1])
 
 df_train = pd.DataFrame({'contents_clean': contents_clean, 'label': df_news['category']})
 print(df_train.tail())
-print (df_train.label.unique())
+print(df_train.label.unique())
 
 label_mapping = {u"汽车": 1, u"财经": 2, u"科技": 3, u"健康": 4, u"体育": 5, u"教育": 6, u"文化": 7, u"军事": 8, u"娱乐": 9, u"时尚": 0}
 tmp = df_train['label']
 df_train['label'] = list(map(lambda x: label_mapping.get(x), df_train['label']))
 # df_train['label'] = df_train['label'].map(label_mapping)
-print (df_train.head())
+print(df_train.head())
 
 from sklearn.model_selection import train_test_split
 
@@ -105,16 +106,16 @@ x_train, x_test, y_train, y_test = train_test_split(df_train['contents_clean'].v
                                                     random_state=12)
 
 # x_train = x_train.flatten()
-print (x_train[0][1])
+print(x_train[0][1])
 words = []
 for line_index in range(len(x_train)):
     try:
         # x_train[line_index][word_index] = str(x_train[line_index][word_index])
         words.append(' '.join(x_train[line_index]))
     except:
-        print (line_index, words[line_index])
-print (words[0])
-print (len(words))
+        print(line_index, words[line_index])
+print(words[0])
+print(len(words))
 
 # 贝叶斯分类器
 from sklearn.feature_extraction.text import CountVectorizer
@@ -140,10 +141,10 @@ for line_index in range(len(x_test)):
         # x_train[line_index][word_index] = str(x_train[line_index][word_index])
         test_words.append(' '.join(x_test[line_index]))
     except:
-        print (line_index, test_words[line_index])
+        print(line_index, test_words[line_index])
 test_words[0]
 
-print (classifier.score(vec.transform(test_words), y_test))
+print(classifier.score(vec.transform(test_words), y_test))
 
 from sklearn.feature_extraction.text import TfidfVectorizer, TfidfTransformer
 
@@ -165,6 +166,6 @@ predicted = classifier.predict(vectorizer.transform(docs_new))
 for text, c in zip(docs_new, predicted):
     for k, v in label_mapping.items():
         if c == v:
-            print (k + "类==>  " + text)
+            print(k + "类==>  " + text)
 
 rest = vectorizer.transform(words)
